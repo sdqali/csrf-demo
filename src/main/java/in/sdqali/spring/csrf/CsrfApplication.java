315822dbc1699a61b727436c9b4d13deedbc723c
Sadique Ali <sadiqalikm@gmail.com>
2016-07-17 15:12:54 -0700

    Fix package names.

--- src/main/java/in/sdqali/spring/csrf/CsrfApplication.java
+++ src/main/java/in/sdqali/spring/csrf/CsrfApplication.java
@@ -1,4 +1,4 @@
-package in.sdqali.spring;
+package in.sdqali.spring.csrf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/in/sdqali/spring/csrf/CsrfApplicationTests.java
+++ src/test/java/in/sdqali/spring/csrf/CsrfApplicationTests.java
@@ -1,4 +1,4 @@
-package in.sdqali.spring;
+package in.sdqali.spring.csrf;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
